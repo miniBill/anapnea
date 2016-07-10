ed5d793c7d5e2a6b3dac2995889519d972c2318f
Leonardo Taglialegne <cmt.miniBill@gmail.com>
2016-07-10 15:55:10 +0200

    Use wget only for retrieving the stage3 tarball

--- roles/gentoo/templates/get_stage.sh
+++ roles/gentoo/templates/get_stage.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 latest="http://distfiles.gentoo.org/releases/amd64/autobuilds/latest-stage3.txt"
-stage3_path=$(curl  $latest 2>&1 \
+stage3_path=$(wget -O- $latest 2>&1 \
             | awk '/\/stage3-amd64-[0-9]+.tar.bz2/{print $1}')
 
 wget  {{ mirror }}/releases/amd64/autobuilds/$stage3_path -O \
